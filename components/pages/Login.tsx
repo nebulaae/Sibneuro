88c03000d808863b114d8b81ee0e194339c38cff
nebulaee <saintx.git@gmail.com>
2026-03-07 01:31:47 +0500

    fix: tg bot

--- components/pages/Login.tsx
+++ components/pages/Login.tsx
@@ -38,7 +38,7 @@ export const Login = () => {
 
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
-    script.setAttribute('data-telegram-login', 'твой_имя_бота'); // ЗАМЕНИ НА СВОЕ
+    script.setAttribute('data-telegram-login', process.env.NEXT_PUBLIC_BOT_USERNAME || '');
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-onauth', 'onTelegramAuth(user)');
     script.setAttribute('data-request-access', 'write');
